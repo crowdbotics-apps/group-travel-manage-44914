dd3c885c5e3c9cf3ce51d900ddf56e8cce2fc839
Crowdbotics <team@crowdbotics.com>
2023-12-08 16:42:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "group-travel-manage-44914",
+  url: "https://group-travel-manage-44914.botics.co",
+  api: "https://group-travel-manage-44914.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
